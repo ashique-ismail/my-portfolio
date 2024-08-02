80c0da8d27ac276c09e59d11e97eb73342612663
Ashique Ismail <ashiqueismail1994@gmail.com>
2024-08-03 00:24:44 +0530

    updated port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const port = 3000
+const port = 8080
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
