414a0c6bc0ea212cad1783b56c1b0f77805ddcbd
Ashique Ismail <ashiqueismail1994@gmail.com>
2024-08-03 00:02:08 +0530

    using express

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const port = 3000
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
+
+app.get('/health-check', (req, res) => {
+  res.send('Application is working..!!!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
